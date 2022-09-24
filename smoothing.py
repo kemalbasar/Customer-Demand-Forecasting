fd2ac6e10c5d714e7a3ba7def6424327c8226df8
reyhanemel <reyhanemel@gmail.com>
2022-09-24 19:51:25 +0300

    statistical and smoothing methods described

--- smoothing.py
+++ smoothing.py
@@ -1,4 +1,6 @@
 import itertools
+import warnings
+
 import statsmodels.api as sm
 from sklearn.metrics import mean_absolute_error
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
@@ -9,22 +11,26 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv(r"C:\Users\kereviz\PycharmProjects\Customer Demand Forecasting\data_sources\raw_data")
+df = pd.read_csv(r"/Users/reyhanmlaydin/Documents/Anaconda_Projects/vbo-cdf/data_sources/ex-raw_data.csv")
 
 df.index = df["Period"]
 df.drop("Period", axis=1, inplace=True)
 df.index = pd.to_datetime(df.index, format='%Y-%m')
+
 df.index.freq = 'MS'
 train = df[df.index < '2022-01']
 test = df[df.index >= '2022-01']
 
 
-# df = df['GRANDTOTAL_NEW'].resample('MS').mean()
 
-# y = y.fillna(y.bfill())
+y = df['GRANDTOTAL'].resample('MS').mean()
+
+y = y.fillna(y.bfill())
 #
-# y.plot(figsize=(15, 6))
-# plt.show()
+y = pd.DataFrame(y)
+
+y.plot(figsize=(15, 6))
+plt.show()
 
 ##################################################
 # Zaman Serisi Yapısal Analizi
@@ -43,7 +49,7 @@ def is_stationary(y):
         print(F"Result: Non-Stationary (H0: non-stationary, p-value: {round(p_value, 3)})")
 
 
-# is_stationary(y)
+is_stationary(y)
 
 # Zaman Serisi Bileşenleri ve Durağanlık Testi
 
@@ -108,7 +114,7 @@ plot_co2(train, test, y_pred, "Single Exponential Smoothing")
 # Hyperparameter Optimization
 ############################
 
-alphas = betas = gammas = np.arange(0.20, 1, 0.10)
+alphas = betas = gammas = np.arange(0.1, 1, 0.1)
 
 abg = list(itertools.product(alphas, betas, gammas))
 
@@ -145,3 +151,179 @@ y_pred = final_tes_model.forecast(8)
 # best_alpha: 0.2 best_beta: 0.4 best_gamma: 0.2 best_mae: 69516.2182
 
 plot_co2(train, test, y_pred, "Triple Exponential Smoothing")
+
+
+
+###########################
+# Statistical Methods
+############################
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+warnings.filterwarnings('ignore')
+
+##################################################
+# ARIMA(p, d, q): (Autoregressive Integrated Moving Average)
+##################################################
+
+arima_model = ARIMA(train, order=(1, 1, 1)).fit(disp=0)
+
+arima_model.summary()
+
+y_pred = arima_model.forecast(8)[0]
+y_pred = pd.Series(y_pred, index=test.index)
+
+def plot_co2(train, test, y_pred, title):
+    mae = mean_absolute_error(test, y_pred)
+    train["1985":].plot(legend=True, label="TRAIN", title=f"{title}, MAE: {round(mae,2)}")
+    test.plot(legend=True, label="TEST", figsize=(6, 4))
+    y_pred.plot(legend=True, label="PREDICTION")
+    plt.show()
+
+plot_co2(train, test, y_pred, "ARIMA")
+
+
+############################
+# AIC & BIC İstatistiklerine Göre Model Derecesini Belirleme
+############################
+
+p = d = q = range(0, 4)
+pdq = list(itertools.product(p, d, q))
+
+
+def arima_optimizer_aic(train, orders):
+    best_aic, best_params = float("inf"), None
+    for order in orders:
+        try:
+            arima_model_result = ARIMA(train, order).fit(disp=0)
+            aic = arima_model_result.aic
+            if aic < best_aic:
+                best_aic, best_params = aic, order
+            print('ARIMA%s AIC=%.2f' % (order, aic))
+        except:
+            continue
+    print('Best ARIMA%s AIC=%.2f' % (best_params, best_aic))
+    return best_params
+
+best_params_aic = arima_optimizer_aic(train, pdq)
+
+############################
+# Final Model
+############################
+
+arima_model = ARIMA(train, best_params_aic).fit(disp=0)
+y_pred = arima_model.forecast(48)[0]
+
+y_pred = pd.Series(y_pred, index=test.index)
+
+plot_co2(train, test, y_pred, "ARIMA")
+
+
+##################################################
+# SARIMA(p, d, q): (Seasonal Autoregressive Integrated Moving-Average)
+##################################################
+
+model = SARIMAX(train, order=(1, 0, 1), seasonal_order=(0, 0, 0, 12))
+
+sarima_model = model.fit(disp=0)
+
+y_pred_test = sarima_model.get_forecast(steps=48)
+
+y_pred = y_pred_test.predicted_mean
+
+y_pred = pd.Series(y_pred, index=test.index)
+
+plot_co2(train, test, y_pred, "SARIMA")
+
+
+############################
+# Hyperparameter Optimization (Model Derecelerini Belirleme)
+############################
+
+p = d = q = range(0, 2)
+pdq = list(itertools.product(p, d, q))
+seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+
+
+def sarima_optimizer_aic(train, pdq, seasonal_pdq):
+    best_aic, best_order, best_seasonal_order = float("inf"), None, None
+    for param in pdq:
+        for param_seasonal in seasonal_pdq:
+            try:
+                sarimax_model = SARIMAX(train, order=param, seasonal_order=param_seasonal)
+                results = sarimax_model.fit(disp=0)
+                aic = results.aic
+                if aic < best_aic:
+                    best_aic, best_order, best_seasonal_order = aic, param, param_seasonal
+                print('SARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, aic))
+            except:
+                continue
+    print('SARIMA{}x{}12 - AIC:{}'.format(best_order, best_seasonal_order, best_aic))
+    return best_order, best_seasonal_order
+
+best_order, best_seasonal_order = sarima_optimizer_aic(train, pdq, seasonal_pdq)
+
+
+############################
+# Final Model
+############################
+
+model = SARIMAX(train, order=best_order, seasonal_order=best_seasonal_order)
+sarima_final_model = model.fit(disp=0)
+
+y_pred_test = sarima_final_model.get_forecast(steps=48)
+
+y_pred = y_pred_test.predicted_mean
+y_pred = pd.Series(y_pred, index=test.index)
+
+plot_co2(train, test, y_pred, "SARIMA")
+
+##################################################
+# BONUS: MAE'ye Göre SARIMA Optimizasyonu
+##################################################
+
+p = d = q = range(0, 2)
+pdq = list(itertools.product(p, d, q))
+seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+
+
+def sarima_optimizer_mae(train, pdq, seasonal_pdq):
+    best_mae, best_order, best_seasonal_order = float("inf"), None, None
+    for param in pdq:
+        for param_seasonal in seasonal_pdq:
+            try:
+                model = SARIMAX(train, order=param, seasonal_order=param_seasonal)
+                sarima_model = model.fit(disp=0)
+                y_pred_test = sarima_model.get_forecast(steps=48)
+                y_pred = y_pred_test.predicted_mean
+                mae = mean_absolute_error(test, y_pred)
+                if mae < best_mae:
+                    best_mae, best_order, best_seasonal_order = mae, param, param_seasonal
+                print('SARIMA{}x{}12 - MAE:{}'.format(param, param_seasonal, mae))
+            except:
+                continue
+    print('SARIMA{}x{}12 - MAE:{}'.format(best_order, best_seasonal_order, best_mae))
+    return best_order, best_seasonal_order
+
+best_order, best_seasonal_order = sarima_optimizer_mae(train, pdq, seasonal_pdq)
+
+model = SARIMAX(train, order=best_order, seasonal_order=best_seasonal_order)
+sarima_final_model = model.fit(disp=0)
+
+y_pred_test = sarima_final_model.get_forecast(steps=48)
+y_pred = y_pred_test.predicted_mean
+y_pred = pd.Series(y_pred, index=test.index)
+
+plot_co2(train, test, y_pred, "SARIMA")
+
+
+############################
+# Final Model
+############################
+
+model = SARIMAX(y, order=best_order, seasonal_order=best_seasonal_order)
+sarima_final_model = model.fit(disp=0)
+
+feature_predict = sarima_final_model.get_forecast(steps=6)
+feature_predict = feature_predict.predicted_mean
+
+
